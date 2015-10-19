bd82870255e2ff4a078d3c28530bdab0cb3eb091
Martin Bontrager <mbontrager@gmail.com>
2015-10-19 16:28:17 -0500

    Delete analysis files

--- scripts/phyloseq_analyses.R
+++
@@ -1,7 +0,0 @@
-## 16S analysis with Phyloseq and ggplot2. Installation instructions are online
-library("phyloseq"); packageVersion("phyloseq")
-library("ggplot2"); packageVersion("ggplot2")
-theme_set(theme_bw())
-
-myData = import_biom("D:/microbiome5-1-14/otu.biom", parseFunction = parse_taxonomy_greengenes)
-plot_bar(myData, fill = "Class")
